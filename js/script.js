c9d3a1bda5403361b61c05bfb2feec56a3c2d9d4
fabio silva <109297324+FabioSilva11@users.noreply.github.com>
2022-07-14 11:26:38 -0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+var meuBotão = document.getElementById('showButton'); meuBotão.onclick = function(){
+
+setTimeout(function() {
+
+console.log('5 seconds have passed') 
+location.replace("https://mpago.la/2GBCy8x");
+
+}, 500);
+}
